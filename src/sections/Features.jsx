dc58a1b796692d6fa6758fd5f88f0b71b1792b4a
noor alam <51040978+noorshanu@users.noreply.github.com>
2024-03-14 11:07:29 +0530

    nw

--- src/sections/Features.jsx
+++ src/sections/Features.jsx
@@ -46,9 +46,11 @@ function Features() {
         <div>
           <h2 className=" text-center font-daughter text-xl pb-2">Telegram Game On Steroids</h2>
 
-          <div className=" h-[300px] w-[300px] bg-black border-2 border-white"></div>
+          <div className=" h-[300px] w-[300px] bg-black border-2 border-white">
+            <img src="images/feat1.png" alt="" className="  object-cover  h-[260px]" />
+          </div>
 
-          <p className=" max-w-80 mx-auto font-daughter text-base">
+          <p className=" max-w-80 mx-auto font-daughter text-base py-2">
             RPS (Rock, Paper, Scissors), but make it epic with Milo 404. Battle,
             win, and vibe with your crypto crew on Telegram, all while bagging
             some sick NFT loot. It’s not just a game; it’s your new favorite
@@ -57,20 +59,24 @@ function Features() {
         </div>
 
         <div>
-          <h2 className=" text-center font-daughter text-xl pb-2">NFT Wonderland </h2>
+          <h2 className=" text-center font-daughter text-xl py-2">NFT Wonderland </h2>
 
-          <div className=" h-[300px] w-[300px] bg-black border-2 border-white"></div>
+          <div className=" h-[300px] w-[300px] bg-black border-2 border-white">
+            <img src="images/fet3.jpg" alt=""  className="  object-cover  h-[300px]"/>
+          </div>
 
-          <p className=" max-w-80 mx-auto font-daughter text-base">
+          <p className=" max-w-80 mx-auto font-daughter text-base py-2">
           Enter the realm of Milo 404 NFTs, where your digital treasures unlock worlds of possibility. More than mere pixels, these gems get you VIP passes to the coolest crypto circuses in town. Art? Check. Rewards? Double check. Your ticket to the metaverse? Triple check.
           </p>
         </div>
         <div>
           <h2 className=" text-center font-daughter text-xl pb-2">The Culture Coin of Solana</h2>
 
-          <div className=" h-[300px] w-[300px] bg-black border-2 border-white"></div>
+          <div className=" h-[300px] w-[300px] bg-black border-2 border-white">
+            <img src="images/fet2.jpg" alt="" className=" object-cover  h-[300px]" />
+          </div>
 
-          <p className=" max-w-80 mx-auto font-daughter text-base">
+          <p className=" max-w-80 mx-auto font-daughter text-base py-2">
           We're crafting more than a meme coin; we're cultivating a culture. With Milo 404, every hodler has a voice, and every opinion matters. It’s democracy, decentralization, and dank memes all rolled into one. Welcome to the family.
           </p>
         </div>
